63d8c2869b90108804bd0c2a1a2bfa229503c3ad
mireleseparovic <150532912+mireleseparovic@users.noreply.github.com>
2024-02-12 08:54:14 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(registration => {
-      console.log('Service Worker registrado com sucesso:', registration);
-    })
-    .catch(error => {
-      console.log('Erro ao registrar Service Worker:', error);
-    });
-}
